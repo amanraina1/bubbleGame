7b67fb605cda498e21832f46e0b452eef2df3cad
amanraina1 <amanraina9086@gmail.com>
2024-01-01 14:41:53 +0530

    completed writing logic

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-var timer = 60;
+var timer = 10;
+var score = 0;
+var hitrn = 0;
 
+function increaseScore() {
+  score += 10;
+  document.querySelector("#scoreVal").textContent = score;
+}
 function getNewHit() {
-  var rn = Math.floor(Math.random() * 10);
-  document.querySelector("#hitVal").textContent = rn;
+  hitrn = Math.floor(Math.random() * 10);
+  document.querySelector("#hitVal").textContent = hitrn;
 }
 function makeBubble() {
   var clutter = "";
@@ -20,9 +26,21 @@ function runTimer() {
       document.querySelector("#timerVal").textContent = timer;
     } else {
       clearInterval(timerInt);
+      document.querySelector("#pbtm").innerHTML = `
+      <h1>GAME OVER !!!!</h1>
+      <h1>YOUR SCORE IS : ${score}</h1>
+      `;
     }
   }, 1000);
 }
+document.querySelector("#pbtm").addEventListener("click", (e) => {
+  var clickedNum = Number(e.target.textContent);
+  if (clickedNum === hitrn) {
+    increaseScore();
+    makeBubble();
+    getNewHit();
+  }
+});
 
 makeBubble();
 runTimer();
